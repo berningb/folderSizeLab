f7dbb646d290b817db9d493f5a4a655d6cc7bfc4
Cody Clawson <cclawson@student.neumont.edu>
2015-12-03 16:39:45 -0700

    added treefile

---
+++ FileTree/FileTree/TreeFile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTree
+{
+    class TreeFile
+    {
+
+
+
+        public TreeFile(DirectoryInfo d)
+        {
+            this.directory = d;
+            this.name = d.Name;
+            this.size = getsize(d);
+        }
+
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private long size;
+
+        public long Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        private DirectoryInfo directory;
+
+        public DirectoryInfo Directory
+        {
+            get { return directory; }
+            set { directory = value; }
+        }
+
+
+        public long getsize(DirectoryInfo d)
+        {
+
+            long size = 0;
+
+            foreach (FileInfo file in d.GetFiles("*", SearchOption.AllDirectories))
+            {
+                size += file.Length;
+            }
+
+
+            return size;
+
+        }
+    }
+}
